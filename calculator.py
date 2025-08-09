38ef9e5aecb6fce7e324dbc9e4bbb0a3348e9299
I181-blip <msiffatjabeen@gmail.com>
2025-08-09 22:05:40 +0500

    Add files via upload
    python and data analysis

---
+++ calculator.py
@@ -0,0 +1,19 @@
+# x=int(input("what is x?"))
+# y=int(input("what is y?"))
+
+
+# if x<y:
+#     print(x,"is less than,y")
+# elif x>y:
+#     print(x,"is greater than",y)
+# elif x==y:
+#     print("x and y are equal")
+
+x=int(input("what is x?"))
+
+def is_even(n):
+  Print ("true") if n%2==0 else print ("false")
+  
+
+
+ 
